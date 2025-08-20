232fadf62f9ab491d0d8b99966e8474f2203be8b
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-08-20 15:06:18 +0530

    First Commit

---
+++ chat-backend/src/main/java/com/echoroom/chat_service/EchoRoomApplication.java
@@ -0,0 +1,13 @@
+package com.echoroom.chat_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoRoomApplication.class, args);
+	}
+
+}
---
+++ chat-backend/src/test/java/com/echoroom/chat_service/EchoRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echoroom.chat_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
